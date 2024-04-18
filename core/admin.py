e769590afd96dff733f5f0e8084b4fdc663f66a8
DavisWere <devisodhis10@gmail.com>
2024-04-18 14:19:21 +0300

    register user

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+from core.models import   User
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Other Fields",
+            {
+                "fields": (
+                   
+                   
+                )
+            },
+        ),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Other Fields",
+            {
+                "fields": (
+                    
+                    "email",
+                   
+
+                )
+            },
+        ),
+    )
+
+admin.site.register(User, CustomUserAdmin)
